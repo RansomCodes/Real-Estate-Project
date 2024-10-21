46d3ca63043a1298aaa20405dd14e0445dd40f16
Manmeet <manmeet7210@gmail.com>
2024-10-21 18:14:29 +0530

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import About from './pages/About'
 import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 function App() {
-  return <BrowserRouter>
+  return (
+    <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
@@ -16,6 +19,7 @@ function App() {
       <Route path="/profile" element={<Profile/>}/>
     </Routes>
   </BrowserRouter>
+  )
 }
 
 export default App
