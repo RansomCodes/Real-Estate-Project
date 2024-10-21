9dc4068308309b4716b7ec143fd232dfd5a192e2
Manmeet <manmeet7210@gmail.com>
2024-10-21 18:43:49 +0530

    Connected to Db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 const express=require('express');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
 
+dotenv.config();
 const app=express();
+
+mongoose.connect(process.env.mongo)
+    .then(()=>{
+    console.log("CONNECTED TO DB");
+    }).catch((error)=>{
+        console.log("UNABLE TO CONNECT TO DB");
+    })
 
 app.listen(3000,()=>{
     console.log("SERVER IS RUNNING ON PORT 3000");
 })
+
+//DD2J3fCy7b974Azc
